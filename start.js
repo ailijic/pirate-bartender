829a683cbd980d0bf2976ddb8dee975c43da31d3
Andrew Ilijic <ailijic@gmail.com>
2016-10-13 22:26:03 -0400

    create ingredients object

--- start.js
+++ start.js
@@ -1,11 +1,18 @@
 export default function myClass() {
   'use strict'
-  const questions = [
-    "Do ye like yer drinks strong?",
-    "Do ye like it with a salty tang?",
-    "Are ye a lubber who likes it bitter?",
-    "Would ye like a bit of sweetness with yer poison?",
-    "Are ye one for a fruity finish?"
-  ]
+  const questions = {
+    strong: "Do ye like yer drinks strong?",
+    salty: "Do ye like it with a salty tang?",
+    bitter: "Are ye a lubber who likes it bitter?",
+    sweet: "Would ye like a bit of sweetness with yer poison?",
+    fruity: "Are ye one for a fruity finish?"
+  }
+  const ingredients = {
+    strong: ['glug of rum', 'slug of whisky', 'splash of gin'],
+    salty: ['olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
+    bitter: ['shake of bitters', 'splash of tonic', 'twist of lemon peel'],
+    sweet: ['sugar cube', 'spoonful of honey', 'splash of cola'],
+    fruity: ['slice of orange', 'dash of cassis', 'cherry on top']
+  }
   
 }
