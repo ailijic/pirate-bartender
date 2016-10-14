2606863f455ef7bd37066c62c6c6360fdeda7b68
Andrew Ilijic <ailijic@gmail.com>
2016-10-13 21:45:48 -0400

    add questions to start.js

---
+++ start.js
@@ -0,0 +1,11 @@
+export default function myClass() {
+  'use strict'
+  const questions = [
+    "Do ye like yer drinks strong?",
+    "Do ye like it with a salty tang?",
+    "Are ye a lubber who likes it bitter?",
+    "Would ye like a bit of sweetness with yer poison?",
+    "Are ye one for a fruity finish?"
+  ]
+  
+}
